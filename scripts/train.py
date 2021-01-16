321ee4bb4cfe1b8ec4a51a7a0af1852d0f080c1e
Raphael Hohmann <raphael.hohmann@tuta.io>
2021-01-16 22:50:54 +0100

    added grid seatch, updated configs

--- scripts/train.py
+++ scripts/train.py
@@ -1,7 +1,10 @@
+import itertools
 import os
 import os.path as osp
+import yaml
 
 import sacred
+from sacred import SETTINGS, Experiment
 from mot_neural_solver.path_cfg import OUTPUT_PATH
 from mot_neural_solver.pl_module.pl_module import MOTNeuralSolver
 from mot_neural_solver.utils.evaluation import MOTMetricsLogger
@@ -13,18 +16,17 @@ from mot_neural_solver.utils.misc import (
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks.lr_logger import LearningRateLogger
 from pytorch_lightning.loggers import TensorBoardLogger
-from sacred import SETTINGS, Experiment
 
 # from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 
 SETTINGS.CONFIG.READ_ONLY_CONFIG = False
+CONFIG_FILE = "configs/tracking_cfg.yaml"
 
 ex = Experiment()
-ex.add_config("configs/tracking_cfg_combined.yaml")
+ex.add_config(CONFIG_FILE)
 ex.add_config(
     {"run_id": "train_w_default_config", "add_date": True, "cross_val_split": None}
 )
-
 
 @ex.config
 def cfg(cross_val_split, eval_params, dataset_params, graph_model_params, data_splits):
@@ -60,8 +62,7 @@ def cfg(cross_val_split, eval_params, dataset_params, graph_model_params, data_s
         data_splits["val"] = []
         eval_params["val_percent_check"] = 0
 
-
-@ex.automain
+@ex.main
 def main(_config, _run):
     sacred.commands.print_config(_run)
     seed_everything(12345)
@@ -106,3 +107,69 @@ def main(_config, _run):
         deterministic=True,
     )
     trainer.fit(model)
+
+def grid_search(conf):
+    """
+    Performs grid search on some of the hyperparameters, as specified in tracking_cfg.yaml.
+    """
+    grid_search = conf["grid_search"]
+    ranges = []
+    for p, l in grid_search.items():
+        ranges.append(range(len(l)))
+
+    # get all possible combinations of params
+    combinations = output_list = list(itertools.product(*ranges))
+
+    for comb in combinations:
+        params = list(grid_search.keys())
+        param_pairs = {}
+        for i in range(len(comb)):
+            param_pairs[params[i]] = grid_search[params[i]][comb[i]]
+        yield param_pairs
+
+# dirty hack to allow grid_search to be specified in same config file as the experiment
+def read_config(config_file):
+    with open(config_file, "r") as f:
+            return yaml.safe_load(f)
+
+def updatedict(d, key, val):
+    """
+    Recursively iterates through a dict and updates a given key's value.
+    """
+    new_d = {}
+    for k, v in d.items():
+        if key == k:
+            new_d[key] = val
+        elif isinstance(v, dict):
+            new_d[k] = updatedict(v, key, val)
+        else:
+            new_d[k] = v
+
+    return new_d
+
+def run(ex):
+    """
+    Runs experiment with regard to run seperate experiments for parameter searches.
+    """
+    conf = read_config(CONFIG_FILE)
+    # check if parameter search is wanted
+    param_search = conf["parameter_search"]
+
+    if param_search is False:
+        ex.run()
+    else:
+        assert param_search.lower() in ["grid_search"], f"Unsupported parameter search: '{param_search}'"
+
+        new_conf = conf
+        if param_search == "grid_search":
+            for new_params in grid_search(conf):
+                # update params in config
+                for k, v in new_params.items():
+                    print(f"RUNNING WITH --> {k}: {v}")
+                    new_conf = updatedict(new_conf, k, v)
+
+                # run experiment with updated config from the parameter search
+                run = ex.run(config_updates=new_conf)
+
+if __name__ == "__main__":
+    run(ex)
